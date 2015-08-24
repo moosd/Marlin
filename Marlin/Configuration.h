cb7ff2c7f2aab6674b98d0cd111455ff5ecf2a0f
moosd <souramoo@gmx.com>
2015-08-24 12:06:51 +0100

    Fixed z software endstops

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -339,7 +339,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define Y_MAX_POS 190
 #define Y_MIN_POS 0
 #define Z_MAX_POS 400
-#define Z_MIN_POS -400
+#define Z_MIN_POS 0
 
 #define X_MAX_LENGTH (X_MAX_POS - X_MIN_POS)
 #define Y_MAX_LENGTH (Y_MAX_POS - Y_MIN_POS)
